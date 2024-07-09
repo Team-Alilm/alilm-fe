f89969f8881774bdb80bde7ca1939ade5e30d0e0
jooyoung.jang <jooyoung.jang@skylabs.io>
2024-07-10 08:54:56 +0900

    feat: api_list 객체로 묶었어요

--- src/constants/apiPath.ts
+++ src/constants/apiPath.ts
@@ -2,20 +2,11 @@ const API_URL = process.env.ALILM_SERVER_API_BASE_URL;
 
 const OAUTH_ROOT = `${API_URL}/oauth2`;
 
-// login api
-export const KAKAO_AUTH_URL_GET = `${OAUTH_ROOT}/authorization/kakao`;
-
-// 서버 상태 체크 api
-export const HEALTH_CHECK_URL_GET = `${API_URL}/health-check`;
-
-// 상품 스크래핑 api
-export const PRODUCT_SCRAPING_URL_GET = `${API_URL}/api/v1/products/scraping`;
-
-// 상품 리스트 API
-export const PRODUCT_LIST_URL_GET = `${API_URL}/api/v1/products`;
-
-// 나의 상품 리스트 API
-export const MY_PRODUCT_LIST_URL_GET = `${API_URL}/api/v1/baskets/my`;
-
-// 상품 등록 API
-export const PRODUCT_REGISTER_URL_POST = `${API_URL}/api/v1/alilms`;
+export const API_LIST = {
+  KAKAO_AUTH: `${OAUTH_ROOT}/authorization/kakao`,
+  HEALTH_CHECK: `${API_URL}/health-check`,
+  PRODUCT_SCRAPING: `${API_URL}/api/v1/products/scraping`,
+  PRODUCT_LIST: `${API_URL}/api/v1/products`,
+  MY_PRODUCT_LIST: `${API_URL}/api/v1/baskets/my`,
+  PRODUCT_REGISTER: `${API_URL}/api/v1/alilms`,
+};
