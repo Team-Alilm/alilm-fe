3666c483d01b4d4835131b2790c3bc71bc2e135f
장주영 <jooyoung.jang@skylabs.io>
2024-08-14 14:02:52 +0900

    feat: 카카오 페이지 이슈 해결
    - Error occurred prerendering page "/oauth/kakao"

--- src/app/oauth/kakao/page.tsx
+++ src/app/oauth/kakao/page.tsx
@@ -1,22 +1,21 @@
 'use client';
 import { useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { LOCAL_STORAGE_KEY, Storage } from '@/libs/storage';
 
 const AauthKakaoPage = () => {
   const router = useRouter();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
-    const accessToken = searchParams.get('Authorization');
+    // URLSearchParams를 사용하여 클라이언트 사이드에서 검색 파라미터를 읽어옵니다.
+    const queryParams = new URLSearchParams(window.location.search);
+    const accessToken = queryParams.get('Authorization');
 
     if (accessToken) {
       Storage.setItem(LOCAL_STORAGE_KEY.accessToken, accessToken);
-
       router.replace('/');
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [router]);
 
   return (
     <div>
