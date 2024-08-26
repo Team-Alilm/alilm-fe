80e24a08e9e550a8e4558a0a6d2ca7cbb5d4b638
김석진 <sj01048260279@gmail.com>
2024-08-27 02:26:26 +0900

    feat: 상품 등록 페이지 CTA Button 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
+'use client';
+
 import { Suspense } from 'react';
+import { useRouter } from 'next/navigation';
+import Button from '@/components/design-system/button';
+import Flex from '@/components/design-system/flex';
 import Spacer from '@/components/design-system/spacer';
 import AlilmInfo from '@/components/main/alilm-info';
 import AlilmTabs from '@/components/main/alilm-tabs';
@@ -8,12 +13,22 @@ import ProductCardList from '@/components/product/product-card-list';
 import * as styles from './page.css';
 
 const MainPage = () => {
+  const router = useRouter();
+
+  const handleMoveCreatePage = () => {
+    router.push('/create');
+  };
+
   return (
     <div className={styles.mainPage}>
       <Spacer height={40} />
       <Suspense>
         <AlilmInfo />
       </Suspense>
+      <Spacer height={60} />
+      <Flex justify="center">
+        <Button onClick={handleMoveCreatePage}>재입고 알림 신청하러가기</Button>
+      </Flex>
       <Spacer height={60} />
       <AlilmTabsProvider>
         <AlilmTabs />
