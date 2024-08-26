6683ca98e2632003f1c6e755d41546d1a2516e8a
김석진 <sj01048260279@gmail.com>
2024-08-27 02:31:29 +0900

    feat: 로고 클릭시 메인 페이지로 이동 기능 추가

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,11 +1,25 @@
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 import * as styles from './index.css';
 
 const Header = () => {
+  const router = useRouter();
+
+  const handleLogoClick = () => {
+    router.push('/');
+  };
+
   return (
     <header className={styles.header}>
-      <Image src="/icons/logo.svg" width={86} height={26} alt="Logo" />
+      <Image
+        src="/icons/logo.svg"
+        onClick={handleLogoClick}
+        style={{ cursor: 'pointer' }}
+        width={86}
+        height={26}
+        alt="Logo"
+      />
       <Image
         src="/icons/alilm.svg"
         className={styles.alilmIcon}
