b47950e27348e20fb7ee84777ef8a4206335ee6a
김석진 <sj01048260279@gmail.com>
2024-08-27 01:05:39 +0900

    fix: useSearchParams 제거

--- src/app/oauth/kakao/page.tsx
+++ src/app/oauth/kakao/page.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { LOCAL_STORAGE_KEY, Storage } from '@/libs/storage';
 
 const OauthKakaoPage = () => {
   const router = useRouter();
-  const searchParams = useSearchParams();
+  const queryParams = new URLSearchParams(window.location.search);
 
   useEffect(() => {
-    const accessToken = searchParams.get('Authorization');
+    const accessToken = queryParams.get('Authorization');
 
     if (accessToken) {
       Storage.setItem(LOCAL_STORAGE_KEY.accessToken, accessToken);
