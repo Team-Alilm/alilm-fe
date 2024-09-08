2047f42a879c3fbb42d78587d63422c0b32b2591
장주영 <jooyoung.jang@skylabs.io>
2024-09-09 08:19:02 +0900

    feat: 전체 레이아웃 width: 100% 변경했어요
    - 메인 페이지 문구 수정했어요

--- src/app/layout.css.ts
+++ src/app/layout.css.ts
@@ -2,7 +2,8 @@ import { style } from '@vanilla-extract/css';
 
 export const layout = style({
   margin: '0 auto',
-  width: '600px',
+  maxWidth: '600px',
+  width: '100%',
   minHeight: '100vh',
   height: '100%',
 });
