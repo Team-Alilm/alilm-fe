1ffa9fd56b1aa7ca12c26645bee8dd142e475658
이수민 <tnklr1111@gmail.com>
2024-09-27 09:56:14 +0900

    feat: fcm 푸시 알림 표시 로직 추가

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -11,7 +11,27 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 // 푸시 내용 처리해서 알림 표시
-self.addEventListener('push', function (event) {});
+// self.addEventListener('push', function (event) {});
+
+self.addEventListener('push', function (event) {
+  if (event.data) {
+    // 알림 메세지일 경우엔 event.data.json().notification;
+    const data = event.data.json().data;
+    console.log('Parsed data:', data);
+    const options = {
+      body: data.body,
+      icon: data.image,
+      image: data.image,
+      data: {
+        click_action: data.click_action,
+      },
+    };
+
+    event.waitUntil(self.registration.showNotification(data.title, options));
+  } else {
+    console.info('This push event has no data.');
+  }
+});
 
 // 클릭 이벤트 처리 - 알림을 클릭 시 사이트로 이동
 self.addEventListener('notificationclick', function (event) {});
