6fea97da8d4ca3c009eea835e60b0701635bd390
장주영 <jooyoung.jang@skylabs.io>
2024-09-27 09:41:46 +0900

    feat: ssr 환경에서 라우터가 발생하지 않도록 수정

--- src/app/oauth/kakao/page.tsx
+++ src/app/oauth/kakao/page.tsx
@@ -9,13 +9,16 @@ const OauthKakaoPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const queryParams = new URLSearchParams(window.location.search);
-    const accessToken = queryParams.get('Authorization');
+    // 클라이언트 환경에서만 실행되도록 window 객체가 있는지 확인
+    if (typeof window !== 'undefined') {
+      const queryParams = new URLSearchParams(window.location.search);
+      const accessToken = queryParams.get('Authorization');
 
-    if (accessToken) {
-      Storage.setItem(LOCAL_STORAGE_KEY.accessToken, accessToken);
-      router.replace('/');
-      handleFcmToken();
+      if (accessToken) {
+        Storage.setItem(LOCAL_STORAGE_KEY.accessToken, accessToken);
+        router.replace('/');
+        handleFcmToken();
+      }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
