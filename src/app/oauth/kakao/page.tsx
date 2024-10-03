e91a8cf8d0cc891530af5ef72c2773a8ebf53679
Jaeha Lee <wogkdkrm112@gmail.com>
2024-10-03 14:41:31 +0900

    fix: router - 의존성 배열 추가

--- src/app/oauth/kakao/page.tsx
+++ src/app/oauth/kakao/page.tsx
@@ -1,27 +1,35 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { LOCAL_STORAGE_KEY, Storage } from '@/libs/storage';
 import handleFcmToken from '@/utils/handle-fcm-token';
 
 const OauthKakaoPage = () => {
   const router = useRouter();
-  const queryParams = new URLSearchParams(window.location.search);
-  const accessToken = queryParams.get('Authorization');
-  console.log('accessToken>>', accessToken);
+  const pathname = usePathname();
+  console.log('pathname>>', pathname);
 
   useEffect(() => {
-    if (accessToken) {
-      Storage.setItem(LOCAL_STORAGE_KEY.accessToken, accessToken);
-      router.replace('/');
-      handleFcmToken();
+    // 클라이언트 환경에서만 실행되도록 window 객체가 있는지 확인
+    if (typeof window !== 'undefined') {
+      const queryParams = new URLSearchParams(window.location.search);
+      const accessToken = queryParams.get('Authorization');
+      console.log('accessToken>>', accessToken);
+
+      if (accessToken) {
+        Storage.setItem(LOCAL_STORAGE_KEY.accessToken, accessToken);
+        router.replace('/');
+        handleFcmToken();
+      } else {
+        alert('Failed to get access token');
+        console.error('Failed to get access token');
+      }
     } else {
       alert('Failed to get access token');
-      console.error('Failed to get access token');
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [accessToken]);
+  }, [router]);
 
   return null;
 };
