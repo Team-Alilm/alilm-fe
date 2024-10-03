fa98504d0497267cfd561b19e5c108af1f054d7e
Jaeha Lee <wogkdkrm112@gmail.com>
2024-10-03 14:48:31 +0900

    fix: kakao webview login error test - 4

--- src/app/oauth/kakao/page.tsx
+++ src/app/oauth/kakao/page.tsx
@@ -10,12 +10,31 @@ const OauthKakaoPage = () => {
   const pathname = usePathname();
   console.log('pathname>>', pathname);
 
+  if (typeof window !== 'undefined') {
+    const queryParams = new URLSearchParams(window.location.search);
+    const accessToken = queryParams.get('Authorization');
+    console.log('accessToken>>', accessToken);
+    alert('accessToken>>' + accessToken);
+
+    if (accessToken) {
+      Storage.setItem(LOCAL_STORAGE_KEY.accessToken, accessToken);
+      router.replace('/');
+      handleFcmToken();
+    } else {
+      alert('Failed to get access token');
+      console.error('Failed to get access token');
+    }
+  } else {
+    alert('Failed to get access token');
+  }
+
   useEffect(() => {
     // 클라이언트 환경에서만 실행되도록 window 객체가 있는지 확인
     if (typeof window !== 'undefined') {
       const queryParams = new URLSearchParams(window.location.search);
       const accessToken = queryParams.get('Authorization');
       console.log('accessToken>>', accessToken);
+      alert('accessToken>>' + accessToken);
 
       if (accessToken) {
         Storage.setItem(LOCAL_STORAGE_KEY.accessToken, accessToken);
