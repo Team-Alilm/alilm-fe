bacbc8ef95b3660fae7856bd502acc5cd7fb5ed8
Jaeha Lee <wogkdkrm112@gmail.com>
2024-10-06 20:17:05 +0900

    feat: 상품 name, option 2줄 까지 보여주기

--- src/components/product/basket-card/index.css.ts
+++ src/components/product/basket-card/index.css.ts
@@ -5,54 +5,68 @@ import { style } from '@vanilla-extract/css';
 export const basketCard = style({
   display: 'flex',
   flexDirection: 'column',
+  justifyContent: 'flex-start',
   minWidth: '100px',
   margin: 'auto',
   width: '90%',
   height: '100%',
 });
 
-export const imageWrapper = style({
-  height: '70%',
-  alignContent: 'center',
-});
+export const imageWrapper = style({});
 
 export const thumbnailImage = style({
   width: '100%',
-  maxHeight: '218px',
-  borderRadius: '4px',
+  minHeight: '21.8rem',
+  height: '21.8rem',
+  maxHeight: '21.8rem',
+
+  // height: 'clamp(21.8rem, 21.8rem, 21.8rem)',
+  borderRadius: '0.4rem',
 });
 
 export const name = style({
-  fontSize: '14px',
+  fontSize: '1.4rem',
   fontWeight: '700',
-  marginBottom: '6px',
-  whiteSpace: 'nowrap',
+  marginBottom: '0.6rem',
+  color: tokens.colors.productName,
+
+  // 2줄까지만 보여주고 나머지는 생략
+  display: '-webkit-box',
+  WebkitLineClamp: 2,
+  WebkitBoxOrient: 'vertical',
   overflow: 'hidden',
   textOverflow: 'ellipsis',
-  color: tokens.colors.productName,
+  lineHeight: '1.2',
+  maxHeight: 'calc(14rem * 1.2 * 2)',
 });
 
 export const options = style({
-  fontSize: '13px',
+  fontSize: '1.3rem',
   fontWeight: '400',
-  marginBottom: '12px',
-  whiteSpace: 'nowrap',
+  marginBottom: '1.2rem',
+  color: tokens.colors.productOptions,
+
+  // 2줄까지만 보여주고 나머지는 생략
+  display: '-webkit-box',
+  WebkitLineClamp: 2,
+  WebkitBoxOrient: 'vertical',
   overflow: 'hidden',
   textOverflow: 'ellipsis',
-  color: tokens.colors.productOptions,
+  lineHeight: '1.2',
+  maxHeight: 'calc(1.3rem * 1.2 * 2)',
 });
 
 export const waitTogetherButton = style({
   border: '1px solid #EEEEEE',
-  borderRadius: '4px',
+  borderRadius: '0.4rem',
   height: '33px',
-  padding: '0 14px',
+  padding: '0 1.4rem',
   fontSize: '1.4rem',
   fontWeight: '600',
   color: '#101010',
   background: '#FFFFFF',
   cursor: 'pointer',
-  marginTop: '10px',
+  marginTop: '1rem',
   whiteSpace: 'nowrap',
   overflow: 'hidden',
   textOverflow: 'ellipsis',
