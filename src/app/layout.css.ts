71d32a9ac0176ee09201d5be8bb6ecbcf8167f7d
Jaeha Lee <wogkdkrm112@gmail.com>
2024-10-06 20:40:28 +0900

    feat: layout minWidth: 32rem

--- src/app/layout.css.ts
+++ src/app/layout.css.ts
@@ -2,8 +2,9 @@ import { style } from '@vanilla-extract/css';
 
 export const layout = style({
   margin: '0 auto',
-  maxWidth: '600px',
+  maxWidth: '60prem',
   width: '100%',
+  minWidth: '32rem',
   backgroundColor: '#ffffff',
   minHeight: '100vh',
 });
