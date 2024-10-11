cd704e2592c956d659ac3c306094600809f1b754
이수민 <tnklr1111@gmail.com>
2024-10-11 19:12:46 +0900

    feat: 카카오 인앱 브라우저 진입 시 외부 브라우저로 리다이렉트

--- src/app/alert-provider.tsx
+++ src/app/alert-provider.tsx
@@ -4,18 +4,15 @@ import { type PropsWithChildren } from 'react';
 import Alert from '@/components/common/alert';
 import { useModalStore } from '@/store/use-modal-store';
 
-import * as styles from './layout.css';
-
-const Client = ({ children }: PropsWithChildren) => {
+const AlertProvider = ({ children }: PropsWithChildren) => {
   const { modalType, title, description } = useModalStore();
 
   return (
     <>
+      {children}
       <Alert type={modalType} title={title} description={description} />
-      <div className={styles.mainContent}>{children}</div>
-      <div id="modal-root" style={{ width: 'inherit' }} />
     </>
   );
 };
 
-export default Client;
+export default AlertProvider;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import { type PropsWithChildren } from 'react';
 import { type Metadata } from 'next';
 import { Pretendard } from '@/styles/local-fonts';
-import { Theme } from '@radix-ui/themes';
 
-import Client from './client';
+import AlertProvider from './alert-provider';
+import FromProvider from './from-provider';
 import HeaderController from './header-controller'; // 클라이언트 컴포넌트
 import * as styles from './layout.css';
 import Providers from './providers';
@@ -25,12 +25,15 @@ const RootLayout = ({ children }: PropsWithChildren) => {
     <html lang="ko" className={Pretendard.className}>
       <body>
         <Providers>
-          <Theme>
-            <div className={styles.layout}>
-              <HeaderController />
-              <Client>{children}</Client>
-            </div>
-          </Theme>
+          <FromProvider>
+            <AlertProvider>
+              <div className={styles.layout}>
+                <HeaderController />
+                <div className={styles.mainContent}>{children}</div>
+                <div id="modal-root" style={{ width: 'inherit' }} />
+              </div>
+            </AlertProvider>
+          </FromProvider>
         </Providers>
       </body>
     </html>
