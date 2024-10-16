ab38b3be1524c66f54e9252630579b769dcd1aaa
이수민 <tnklr1111@gmail.com>
2024-10-16 17:37:26 +0900

    fix: basketResponse 필드명 수정

--- src/hooks/quries/use-get-baskets.ts
+++ src/hooks/quries/use-get-baskets.ts
@@ -4,8 +4,8 @@ import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 
 interface BasketsResponse {
   size: number;
-  content: Basket[];
-  last: number;
+  contents: Basket[];
+  last: boolean;
 }
 
 export const BASKETS_QUERY_KEY = 'getBaskets';
@@ -23,7 +23,7 @@ export const useGetBaskets = () => {
 
       return allPages.length;
     },
-    select: data => data.pages.flatMap(({ content }) => content),
+    select: data => data.pages.flatMap(({ contents }) => contents),
     initialPageParam: 0,
   });
 };
