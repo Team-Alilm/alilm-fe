7c546a6e5648a7d289fee35c45a6d46561986176
이수민 <tnklr1111@gmail.com>
2024-11-28 18:08:51 +0900

    feat: 알림 아이콘 설명 배너 추가

--- src/components/product/my-basket-card-list/index.css.ts
+++ src/components/product/my-basket-card-list/index.css.ts
@@ -1,3 +1,4 @@
+import { tokens } from '@/styles';
 import { style } from '@vanilla-extract/css';
 
 export const cardListHome = style({
@@ -16,4 +17,18 @@ export const cardListMypage = style({
   display: 'grid',
   // gridTemplateColumns: 'repeat(1fr)',
   rowGap: '2.4rem',
+});
+
+export const infoBox = style({
+  width: '100%',
+  height: '4.9rem',
+  borderRadius: '0.8rem',
+  backgroundColor: tokens.colors.background,
+  color: '#717171',
+  padding: '1.6rem',
+  fontSize: '1.4rem',
+  marginBottom: '2.4rem',
+  display: 'flex',
+  gap: '0.8rem',
+  alignItems: 'center',
 });
