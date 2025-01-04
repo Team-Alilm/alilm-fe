7fcc630b41d2caf22801c1ccbe628a6be046015c
이수민 <tnklr1111@gmail.com>
2025-01-04 20:03:31 +0900

    fix: 상품 조회 에러 처리 수정

--- src/components/create/product-options-form/index.tsx
+++ src/components/create/product-options-form/index.tsx
@@ -4,8 +4,7 @@ import Select from '@/components/design-system/select';
 import { type RegisteredBasketsParams } from '@/hooks/mutations/use-registered-baskets';
 import { useGetProductsCrawling } from '@/hooks/queries/use-get-products-crawling';
 import { useModalStore } from '@/store/use-modal-store';
-import { ERROR_CODE, ERROR_MESSAGES, type ErrorCode } from '@/utils/error-code';
-import type { AxiosError } from 'axios';
+import { ERROR_CODE } from '@/utils/error-code';
 
 import * as styles from './index.css';
 
@@ -39,11 +38,32 @@ const ProductOptionsForm = ({ url, setCreateForm }: ProductOptionsFormProps) => 
 
   useEffect(() => {
     if (isError) {
-      const errorCode =
-        ((error as Error)?.message as ErrorCode) || ERROR_CODE.INTERNAL_SERVER_ERROR;
-      renderErrorMessage(errorCode);
+      const rawErrorMessage =
+        ((error as Error)?.message as string) || ERROR_CODE.INTERNAL_SERVER_ERROR;
+
+      const processedErrorMessage = extractErrorMessage(rawErrorMessage);
+
+      renderErrorMessage(processedErrorMessage);
     }
   }, [isError, error]);
+
+  const extractErrorMessage = (rawErrorMessage: string): string => {
+    const matches = Array.from(rawErrorMessage.matchAll(/default message \[(.*?)\]/g));
+
+    if (matches.length > 0) {
+      return matches[matches.length - 1][1];
+    } else if (matches.length === 0) {
+      return rawErrorMessage;
+    }
+
+    return ERROR_CODE.INTERNAL_SERVER_ERROR;
+  };
+
+  const renderErrorMessage = (errorMessage: string) => {
+    if (errorMessage) {
+      onOpen({ modalType: 'alert', title: errorMessage });
+    }
+  };
 
   const handleOptionsChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target;
@@ -53,21 +73,6 @@ const ProductOptionsForm = ({ url, setCreateForm }: ProductOptionsFormProps) => 
   if (isPending) return <p className={styles.pending}>상품 정보를 불러오고 있어요...</p>;
 
   if (!product) return null;
-
-  const renderErrorMessage = (errorCode: ErrorCode) => {
-    const message = ERROR_MESSAGES[errorCode] || ERROR_MESSAGES[ERROR_CODE.INTERNAL_SERVER_ERROR];
-    const invalidUrl = (error as AxiosError)?.response?.data;
-
-    if (invalidUrl === 'UNSUPPORTED_URL') {
-      onOpen({
-        modalType: 'alert',
-        title: 'URL 주소를 다시 확인해주세요.',
-        description: '무신사 상품만 등록할 수 있어요.',
-      });
-    } else {
-      onOpen({ modalType: 'alert', title: message });
-    }
-  };
 
   return (
     <>
