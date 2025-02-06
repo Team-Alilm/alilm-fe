1b9642f542cbd1836e5f74633d21131cd63ae0d5
aihj <heejae9102@gmail.com>
2025-02-06 14:57:17 +0900

    feat: 알림여부별 아이콘 반영 #158

--- src/components/product/product-thumbnail/index.tsx
+++ src/components/product/product-thumbnail/index.tsx
@@ -61,7 +61,7 @@ const ProductThumbnailImage = ({
             height={800}
             priority
           />
-          {!isMessageSent ? null : (
+          {isMessageSent && (
             <div className={styles.iconWrapper}>
               <BellRing width={20} height={20} stroke="rgba(135, 96, 225, 0.8)" />
             </div>
@@ -73,33 +73,6 @@ const ProductThumbnailImage = ({
               stroke="rgba(228, 157, 16, 0.9)"
               fill="rgba(228, 157, 16, 0.9)"
             />
-            {counts}
-          </div>
-        </>
-      )}
-
-      {validURL && isMessageSent && (
-        <>
-          <Image
-            src={validURL}
-            className={styles.thumbnailImage({ card })}
-            alt="Basket Thumbnail"
-            layout="responsive"
-            width={800}
-            height={800}
-            priority
-          />
-          <div className={styles.iconWrapper}>
-            <BellRing width={20} height={20} stroke="rgba(135, 96, 225, 0.8)" />
-          </div>
-          <div className={styles.iconWrapper1}>
-            <Users
-              width={12}
-              height={12}
-              stroke="rgba(228, 157, 16, 0.9)"
-              fill="rgba(228, 157, 16, 0.9)"
-            />
-            {/* <Icon icon="UserTwoPerson" width={12} height={12} color="orange" /> */}
             {counts}
           </div>
         </>
