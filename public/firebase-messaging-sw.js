9c74f2e8875f546287a146e9dc6f3c60fb5a46ee
jangjuyeong <cloudwi@naver.com>
2025-02-12 15:29:49 +0900

    Revert "feat: fcm test"
    This reverts commit 468f4ea86a2326f4aba79fdacc3a8668acf317d7.

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -8,6 +8,32 @@ firebase.initializeApp({
 });
 const messaging = firebase.messaging();
 
+self.addEventListener('push', function (event) {
+  if (event.data) {
+    const data = event.data.json().data;
+    const options = {
+      body: data.body,
+      icon:
+        data.icon ||
+        'https://file.notion.so/f/f/c345e317-1a77-4e86-8b67-b491a5db92b8/732799dc-6ad9-46f8-8864-22308c10cdb8/free-icon-bells-7124213.png?table=block&id=1037b278-57a0-8022-8a73-ea04c03ae27e&spaceId=c345e317-1a77-4e86-8b67-b491a5db92b8&expirationTimestamp=1730354400000&signature=hBdHPuerhscY6rXIkAe40sWyyvEq22eyqZ7AqA2Gt5o&downloadName=free-icon-bells-7124213.png',
+      image: data.image,
+      data: {
+        click_action: data.click_action,
+      },
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+  } else {
+    console.info('This push event has no data.');
+  }
+});
+
+// 클릭 이벤트 처리 - 알림을 클릭 시 사이트로 이동
+self.addEventListener('notificationclick', function (event) {
+  const clickActionUrl = event.notification.data.click_action;
+  event.notification.close();
+  event.waitUntil(clients.openWindow(clickActionUrl));
+});
+
 // 백그라운드 메시지 처리
 messaging.onBackgroundMessage(payload => {
   console.log('[firebase-messaging-sw.js] Received background message', payload);
