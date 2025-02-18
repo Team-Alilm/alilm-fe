6f99dfcb5138adbf1586cea3b5dfa94881233f95
이수민 <tnklr1111@gmail.com>
2025-02-19 04:19:27 +0900

    feat: 재입고 알림 히스토리 페이지 구현

---
+++ src/app/notifications/page.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useGetNotifications } from '@/hooks/queries/use-get-notifications';
+import { BellRing } from 'lucide-react';
+
+import {
+  brandName,
+  info,
+  infoWrapper,
+  notification,
+  notificationContent,
+  notificationImage,
+  notificationOptions,
+  notificationProduct,
+  notificationTextWrapper,
+  notificationType,
+  readNotification,
+} from './index.css';
+
+const NotificationList = () => {
+  const { data: notifications } = useGetNotifications();
+
+  const router = useRouter();
+
+  if (!notifications) return;
+
+  return (
+    <div>
+      <div className={infoWrapper}>
+        <div className={info}>
+          <BellRing stroke={'#707070'} width={13} />
+          30일이 지난 알림은 사라져요.
+        </div>
+      </div>
+      {notifications.alimHistoryList.map(item => (
+        <div
+          key={item.alilmId}
+          className={`${notification} ${item.readYn ? readNotification : ''}`}
+        >
+          <div
+            className={notificationContent}
+            onClick={() => router.push(`/product/${item.productid}`)}
+          >
+            <div className={notificationTextWrapper}>
+              <div className={notificationType}>재입고 알림</div>
+              <div className={brandName}>{item.brand}</div>
+              <div className={notificationProduct}>{item.name}</div>
+              {(item.firstOption || item.secondOption || item.thirdOption) && (
+                <div className={notificationOptions}>
+                  {item.firstOption && <span>{item.firstOption}</span>}
+                  {item.secondOption && <span> | {item.secondOption}</span>}
+                  {item.thirdOption && <span> | {item.thirdOption}</span>}
+                </div>
+              )}
+            </div>
+            <Image
+              src={item.imageUrl}
+              alt="Notification Image"
+              width={50}
+              height={50}
+              className={notificationImage}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NotificationList;
