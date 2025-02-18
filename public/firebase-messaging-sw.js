121a12fc46dddf8ec24fa7e950387e9e0cc3224e
cloudwi <cloudwiiiii@gmail.com>
2025-02-18 17:14:16 +0900

    feat : service worker test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -9,3 +9,21 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+self.addEventListener('push', function (event) {
+    if (event.data) {
+        const data = event.data.json();
+        console.log('This push event has data: ', data);
+        const options = {
+            body: data.body,
+            icon: data.image,
+            image: data.image,
+            data: {
+                click_action: data.click_action,
+            },
+        };
+        event.waitUntil(self.registration.showNotification(data.title, options));
+    } else {
+        console.info('This push event has no data.');
+    }
+});
