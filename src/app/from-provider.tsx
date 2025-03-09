d1a6aa7116e66bb9419f0279bc44f492e6403375
jangjuyeong <cloudwi@naver.com>
2025-03-09 21:12:35 +0900

    test: 로그

--- src/app/from-provider.tsx
+++ src/app/from-provider.tsx
@@ -4,6 +4,10 @@ import { type PropsWithChildren, useEffect } from 'react';
 
 const FromProvider = ({ children }: PropsWithChildren) => {
   useEffect(() => {
+    console.log('NEXT_PUBLIC_INAPP_REDIRECT_URL:', process.env.NEXT_PUBLIC_INAPP_REDIRECT_URL);
+  }, []);
+
+  useEffect(() => {
     const useragent = navigator.userAgent.toLowerCase();
     const targetUrl = process.env.NEXT_PUBLIC_INAPP_REDIRECT_URL;
     if (useragent.match(/kakaotalk/i) && targetUrl) {
