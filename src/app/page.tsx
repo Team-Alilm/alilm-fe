d6bc8259c8c3258ffca64c1f1f6f1c0605bbeb7c
aihj <heejae9102@gmail.com>
2025-03-23 18:54:13 +0900

    feat: 홈개편3 PR: feat#177

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -115,7 +115,7 @@ const MainPage = () => {
       <h3 className={styles.late1}>재입고 늦어지는 상품</h3>
       <h5 className={styles.late2}>비슷한 가격대 추천 상품을 살펴보세요</h5>
 
-      <div style={{ display: 'flex', height: '28vh', gap: '1vh', paddingLeft: '1vh' }}>
+      <div style={{ display: 'flex', height: '30vh', gap: '1vh', margin: '0 auto' }}>
         <div style={{ position: 'relative', width: '24vh' }}>
           <ProductCard
             key={undefined}
