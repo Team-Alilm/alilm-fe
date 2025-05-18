32ce1c4df3913f0abf2910c3b9a0d498faa23385
aihj <107903706+aihj@users.noreply.github.com>
2025-05-18 12:41:02 +0900

    fix: 홈화면 알림건수 영역 가운데 정렬 PR: feat/#177
    fix: 홈화면 알림건수 영역 가운데 정렬 PR: feat/#177

--- src/components/main/alilm-info/index.css.ts
+++ src/components/main/alilm-info/index.css.ts
@@ -10,9 +10,11 @@ export const title = style({
   fontSize: '16px',
   fontWeight: '700',
   marginBottom: '12px',
+  textAlign: 'center',
 });
 
 export const figure = style({
   fontSize: '28px',
   fontWeight: '700',
+  textAlign: 'center',
 });
