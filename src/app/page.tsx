9796040cf09c43776854399de1075bd5aaf700d5
이수민 <tnklr1111@gmail.com>
2025-05-20 22:51:52 +0900

    fix:  홈화면 섹션 간격 수정 및디바이더 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,11 +120,12 @@ const MainPage = () => {
           ))}
         </Swiper>
       </div>
-      <div style={{ padding: '2rem', display: 'flex', justifyContent: 'center' }}>
+      <div style={{ padding: '4rem 0 3rem 0', display: 'flex', justifyContent: 'center' }}>
         <Button onClick={handleMoveCreatePage}>재입고 알림 신청하러가기</Button>
       </div>
       {userAccessToken && (
         <div className={styles.secondModule}>
+          <Divider />
           <h3 className={styles.late1}>재입고 늦어지는 상품</h3>
           <h5 className={styles.late2}>비슷한 가격대 추천 상품을 살펴보세요</h5>
           <div style={{ display: 'flex', gap: '1rem', overflow: 'hidden' }}>
