5fb185a465f5e3017819fcc9949aaf3d02d73e9f
이수민 <tnklr1111@gmail.com>
2025-05-20 23:07:27 +0900

    fix:  홈화면 섹션 간격 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -120,70 +120,72 @@ const MainPage = () => {
           ))}
         </Swiper>
       </div>
-      <div style={{ padding: '4rem 0 3rem 0', display: 'flex', justifyContent: 'center' }}>
+      <div style={{ paddingTop: '2rem', display: 'flex', justifyContent: 'center' }}>
         <Button onClick={handleMoveCreatePage}>재입고 알림 신청하러가기</Button>
       </div>
       {userAccessToken && (
-        <div className={styles.secondModule}>
-          <Divider />
-          <h3 className={styles.late1}>재입고 늦어지는 상품</h3>
-          <h5 className={styles.late2}>비슷한 가격대 추천 상품을 살펴보세요</h5>
-          <div style={{ display: 'flex', gap: '1rem', overflow: 'hidden' }}>
-            {/* 고정된 My상품 카드 */}
-            <div style={{ flex: '0 0 auto' }}>
-              <div className={styles.parent}>
-                <div className={styles.topBadge1}>My상품</div>
+        <>
+          <Divider marginX={5} />
+          <div className={styles.secondModule}>
+            <h3 className={styles.late1}>재입고 늦어지는 상품</h3>
+            <h5 className={styles.late2}>비슷한 가격대 추천 상품을 살펴보세요</h5>
+            <div style={{ display: 'flex', gap: '1rem', overflow: 'hidden' }}>
+              {/* 고정된 My상품 카드 */}
+              <div style={{ flex: '0 0 auto' }}>
+                <div className={styles.parent}>
+                  <div className={styles.topBadge1}>My상품</div>
 
-                {oldResponse?.oldProduct && (
-                  <button
-                    style={{ position: 'relative', all: 'unset' }}
-                    onClick={() => router.push(`/product/${oldResponse.oldProduct.productId}`)}
-                  >
-                    <ProductThumbnailImage
-                      card="slide"
-                      imageUrl={oldResponse.oldProduct.thumbnailUrl ?? ''}
-                    />
-                  </button>
-                )}
-                <div className={styles.iconWrapper}>
-                  <Clock size={13} />
-                  기다린 시간: {timePassed}
-                </div>
-                <p className={styles.name}>{oldResponse?.oldProduct?.brand}</p>
-                <p className={styles.options}>
-                  {oldResponse?.oldProduct?.category} |{' '}
-                  {oldResponse?.oldProduct?.price?.toLocaleString()}원
-                </p>
-              </div>
-            </div>
-
-            {/* 스크롤되는 추천상품 Swiper */}
-            <Swiper
-              mousewheel={true}
-              slidesPerView="auto"
-              modules={[Pagination, Mousewheel]}
-              style={{ paddingBottom: '2rem', flex: '1 1 auto' }}
-            >
-              {related?.map(item => (
-                <SwiperSlide key={item.productId} className={styles.cardWrapper}>
-                  <div className={styles.topBadge2}>추천상품</div>
-
-                  <button
-                    style={{ position: 'relative', all: 'unset' }}
-                    onClick={() => router.push(`/product/${item.productId}`)}
-                  >
-                    <ProductThumbnailImage card="slide" imageUrl={item.thumbnailUrl!} />
-                  </button>
-
-                  <p className={styles.name}>{item.brand}</p>
+                  {oldResponse?.oldProduct && (
+                    <button
+                      style={{ position: 'relative', all: 'unset' }}
+                      onClick={() => router.push(`/product/${oldResponse.oldProduct.productId}`)}
+                    >
+                      <ProductThumbnailImage
+                        card="slide"
+                        imageUrl={oldResponse.oldProduct.thumbnailUrl ?? ''}
+                      />
+                    </button>
+                  )}
+                  <div className={styles.iconWrapper}>
+                    <Clock size={13} />
+                    기다린 시간: {timePassed}
+                  </div>
+                  <p className={styles.name}>{oldResponse?.oldProduct?.brand}</p>
                   <p className={styles.options}>
-                    {item.category} | {item.price.toLocaleString()}원
+                    {oldResponse?.oldProduct?.category} |{' '}
+                    {oldResponse?.oldProduct?.price?.toLocaleString()}원
                   </p>
-                </SwiperSlide>
-              ))}
-            </Swiper>
+                </div>
+              </div>
+
+              {/* 스크롤되는 추천상품 Swiper */}
+              <Swiper
+                mousewheel={true}
+                slidesPerView="auto"
+                modules={[Pagination, Mousewheel]}
+                style={{ paddingBottom: '2rem', flex: '1 1 auto' }}
+              >
+                {related?.map(item => (
+                  <SwiperSlide key={item.productId} className={styles.cardWrapper}>
+                    <div className={styles.topBadge2}>추천상품</div>
+
+                    <button
+                      style={{ position: 'relative', all: 'unset' }}
+                      onClick={() => router.push(`/product/${item.productId}`)}
+                    >
+                      <ProductThumbnailImage card="slide" imageUrl={item.thumbnailUrl!} />
+                    </button>
+
+                    <p className={styles.name}>{item.brand}</p>
+                    <p className={styles.options}>
+                      {item.category} | {item.price.toLocaleString()}원
+                    </p>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
           </div>
-        </div>
+        </>
       )}
       <Spacer height={50} />
       <div>
