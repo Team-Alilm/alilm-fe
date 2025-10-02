1095ed968fc5ee88bd4d795f8523e79963206877
cloudwi <cloudwi@users.noreply.github.com>
2025-10-02 09:10:15 +0900

    fix: products page 공백 추가

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -12,6 +12,7 @@ import * as styles from './page.css';
 
 const ProductsPage = () => {
   const searchParams = useSearchParams();
+
   const category = searchParams.get('category') || '';
   const sort = (searchParams.get('sort') as string) || 'WAITING_COUNT_DESC';
   const search = searchParams.get('search') || '';
